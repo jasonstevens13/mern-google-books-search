7a97c5afce7fd33c177cb685d904ce867456cb05
jasonstevens13 <jasonstevens13@yahoo.com>
2020-07-24 19:42:26 -0400

    created save and view btn components

---
+++ client/src/components/ViewBtn/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+function ViewBtn(props) {
+    return (
+        <button type="button" role="button" className="btn btn-success" {...props} tabIndex="0">
+            View
+        </button>
+    );
+}
+
+export default ViewBtn;
