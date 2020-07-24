027a6aae13252ef7fe3d211ba04a92538bab29d9
jasonstevens13 <jasonstevens13@yahoo.com>
2020-07-24 14:58:17 -0400

    npm install react-bootstrap botstrap; remove link idex.html and add import to the index.js file

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -9,7 +9,7 @@ function Detail(props) {
 
   // When this component mounts, grab the book with the _id of props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
-  const {id} = useParams()
+  const { id } = useParams()
   useEffect(() => {
     API.getBook(id)
       .then(res => setBook(res.data))
@@ -17,34 +17,34 @@ function Detail(props) {
   }, [])
 
   return (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>
-                {book.title} by {book.author}
-              </h1>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-10 md-offset-1">
-            <article>
-              <h1>Synopsis</h1>
-              <p>
-                {book.synopsis}
-              </p>
-            </article>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1>
+              {book.title} by {book.author}
+            </h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-10 md-offset-1">
+          <article>
+            <h1>Synopsis</h1>
+            <p>
+              {book.synopsis}
+            </p>
+          </article>
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-2">
+          <Link to="/">← Back to Authors</Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 
 export default Detail;
