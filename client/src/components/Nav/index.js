b4a15d32fe339ece4a9e1a912325b65b8d9f2172
jasonstevens13 <jasonstevens13@yahoo.com>
2020-07-24 17:59:24 -0400

    edited navbar component to include search and saved links; not yet attached to anything

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
+
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
         Google Books
       </a>
-    </nav>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item text-white">
+            <a className="nav-link">
+              <Link to="/search">
+                <div className="nav-item text-white">
+                  Search
+                </div>
+              </Link>
+            </a>
+          </li>
+          <li className="nav-item text-white">
+            <a className="nav-link">
+              <Link to="/saved">
+                <div className="nav-item text-white">
+                  Saved
+                </div>
+              </Link>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav >
   );
 }
 
